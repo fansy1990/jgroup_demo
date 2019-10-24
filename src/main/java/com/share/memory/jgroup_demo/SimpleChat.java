bd9959a1713e34393d7ce13f078cd2a56d1fbe1e
fansy1990 <fansy1990@foxmail.com>
2019-10-24 23:07:22 +0800

    tcp ping test successfully

---
+++ src/main/java/com/share/memory/jgroup_demo/SimpleChat.java
@@ -0,0 +1,64 @@
+package com.share.memory.jgroup_demo;
+
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+import org.jgroups.conf.ConfiguratorFactory;
+import org.jgroups.conf.ProtocolConfiguration;
+import org.jgroups.conf.ProtocolStackConfigurator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+
+/**
+ * author : fanzhe
+ * email : fansy1990@foxmail.com
+ * date : 2019/10/24 PM10:20.
+ */
+public class SimpleChat extends ReceiverAdapter {
+    JChannel channel;
+    String user_name=System.getProperty("user.name", "n/a");
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
+        new SimpleChat().start();
+    }
+
+    private void start() throws Exception {
+        ProtocolStackConfigurator protocolConfiguration =
+                ConfiguratorFactory.getStackConfigurator("tcp_1.xml");
+
+        channel=new JChannel("tcp_1.xml").setReceiver(this);
+        channel.connect("ChatCluster");
+        eventLoop();
+        channel.close();
+    }
+
+
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        System.out.println(msg.getSrc() + ": " + msg.getObject());
+    }
+    private void eventLoop() {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            try {
+                System.out.print("> "); System.out.flush();
+                String line=in.readLine().toLowerCase();
+                if(line.startsWith("quit") || line.startsWith("exit"))
+                    break;
+                line="[" + user_name + "] " + line;
+                Message msg=new Message(null, line);
+                channel.send(msg);
+            }
+            catch(Exception e) {
+            }
+        }
+    }
+}
---
+++ src/main/java/com/share/memory/jgroup_demo/SimpleChat2.java
@@ -0,0 +1,54 @@
+package com.share.memory.jgroup_demo;
+
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * author : fanzhe
+ * email : fansy1990@foxmail.com
+ * date : 2019/10/24 PM10:20.
+ */
+public class SimpleChat2 extends ReceiverAdapter {
+    JChannel channel;
+    String user_name=System.getProperty("user.name", "n/a");
+
+
+    public static void main(String[] args) throws Exception {
+        new SimpleChat2().start();
+    }
+
+    private void start() throws Exception {
+        channel=new JChannel("tcp_2.xml").setReceiver(this);
+        channel.connect("ChatCluster");
+        eventLoop();
+        channel.close();
+    }
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        System.out.println(msg.getSrc() + ": " + msg.getObject());
+    }
+    private void eventLoop() {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            try {
+                System.out.print("> "); System.out.flush();
+                String line=in.readLine().toLowerCase();
+                if(line.startsWith("quit") || line.startsWith("exit"))
+                    break;
+                line="[" + user_name + "] " + line;
+                Message msg=new Message(null, line);
+                channel.send(msg);
+            }
+            catch(Exception e) {
+            }
+        }
+    }
+}
